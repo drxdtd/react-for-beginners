c110a10e976f0aeb40876f35a5d5b125fee7f2f0
drxdtd <drxdrd@MacBook-Air--Drxdtd.local>
2024-01-08 20:08:55 +0900

    7.3

--- src/App.js
+++ src/App.js
@@ -2,58 +2,40 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [price, setPrice] = useState(0);
-  const [budget, setBudget] = useState(0);
-  const [calculateValue, setCalculateValue] = useState(0);
-
-  const onClick = (event) => {
-    setCalculateValue(Math.round(budget / price));
-  };
-  const onChangeBudget = (event) => {
-    setBudget(event.target.value);
-  };
-  const onChangePrice = (event) => {
-    setPrice(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year `
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers").then(
-      (response) =>
-        response.json().then((json) => {
-          setCoins(json);
-          setPrice(json[0].quotes.USD.price);
-          return;
-        }),
-      setLoading(false)
-    );
+    getMovies();
   }, []);
-
-  const value = Math.round(budget / price);
+  console.log(movies);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>loading...</strong>
+        <h1>loading...</h1>
       ) : (
-        <select onChange={onChangePrice}>
-          {coins.map((coin) => (
-            <option key={coin.id} value={coin.quotes.USD.price}>
-              {coin.name}({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <h2>{movie.title}</h2>
+              <img src={movie.medium_cover_image}></img>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
-      <div>
-        <label htmlFor="Your USD">USD</label>
-        <input
-          id="USD"
-          placeholder="Please write your budget"
-          type="number"
-          onChange={onChangeBudget}
-        />
-        <h1>{calculateValue !== 0 ? `You can get ${calculateValue}` : ""}</h1>
-        <button onClick={onClick}>Calculate</button>
-      </div>
     </div>
   );
 }
